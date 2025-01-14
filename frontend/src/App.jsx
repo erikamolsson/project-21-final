be90e56dd5646b443708b6550ed6499a074a7053
Erika Olsson <erikamolsson87@gmail.com>
2025-01-14 22:37:11 +0100

    - Routes frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChallengeFeed } from "./components/moduls/Feed/ChallengeFeed";
 import { HomepageAll } from "./pages/HomepageAll";
 import { HomepageUser } from "./pages/HomepageUser";
@@ -10,12 +11,16 @@ import { ChallengesForm } from "./pages/ChallengesForm";
 export const App = () => {
 
   return (
-    <>
-      <ChallengesForm />
-      <ProfileUser />
-      <HomepageAll />
-      <HomepageUser />
-      <ChallengeFeed />
-    </>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomepageAll />} />
+        <Route path="/feed" element={<ChallengeFeed />} />
+        <Route path="/challenges-form" element={<ChallengesForm />} />
+        <Route path="/user-start" element={<HomepageUser />} />
+        <Route path="/profile" element={<ProfileUser />} />
+      </Routes>
+    </Router>
+    
   );
 };
